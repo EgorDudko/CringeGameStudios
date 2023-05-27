3e1678f50c116b4efcb3003766469a81edf7672c
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-05-27 03:11:03 +0300

    Merging with assets

--- MainProject/PracticeClicker/Assets/Scripts/Core/TruckManager.cs
+++ MainProject/PracticeClicker/Assets/Scripts/Core/TruckManager.cs
@@ -9,6 +9,7 @@ public class TruckManager : MonoBehaviour
     [SerializeField] private int _maxBoxCount;
     [SerializeField] private Transform _outsidePosition;
     [SerializeField] private Transform _insidePosition;
+    [SerializeField] private Storage _storage;
 
     private int _boxCount;
     private bool _isMoving;
@@ -45,9 +46,10 @@ public class TruckManager : MonoBehaviour
         }
         _isMoving = false;
     }
+
     public void UpgradeCapacity()
     {
-        
+        _maxBoxCount = _storage.truckCapacityUpgrades[_storage.truckCapacityLevel];
     }
 
     private void OnTriggerEnter(Collider other)
