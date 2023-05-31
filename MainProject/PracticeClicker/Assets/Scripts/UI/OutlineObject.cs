1d733e101e6ebb37529227fdbc2ee03852fc2d61
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-05-31 18:56:52 +0300

    Added signs models

--- MainProject/PracticeClicker/Assets/Scripts/UI/OutlineObject.cs
+++ MainProject/PracticeClicker/Assets/Scripts/UI/OutlineObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OutlineObject : MonoBehaviour
 {
     [SerializeField] private MeshRenderer[] _meshRenderers;
+    [SerializeField] private int _materialIndex;
 
     private Color[] _deafultColors;
 
@@ -13,7 +14,7 @@ public class OutlineObject : MonoBehaviour
         _deafultColors = new Color[_meshRenderers.Length];
         for (int i = 0; i < _meshRenderers.Length; i++) 
         {
-            _deafultColors[i] = _meshRenderers[i].material.color;
+            _deafultColors[i] = _meshRenderers[i].materials[_materialIndex].color;
         }
     }
 
@@ -22,18 +23,21 @@ public class OutlineObject : MonoBehaviour
         for (int i = 0; i < _meshRenderers.Length; i++)
         {
             if (_meshRenderers[i] == null) return;
-            _meshRenderers[i].material.color = _deafultColors[i];
+            _meshRenderers[i].materials[_materialIndex].color = _deafultColors[i];
         }
     }
 
     private void OnMouseOver()
     {
-        if (_meshRenderers[0].material.color.r - _deafultColors[0].r >= 0.3f) return;
+        if (_meshRenderers[0].materials[_materialIndex].color.r - _deafultColors[0].r >= 0.3f) return;
 
         for (int i = 0; i < _meshRenderers.Length; i++)
         {
-            if (_meshRenderers[i].material.color.r - _deafultColors[i].r < 0.3f) 
-                _meshRenderers[i].material.color = new Color(_meshRenderers[i].material.color.r + 0.01f, _meshRenderers[i].material.color.g + 0.01f, _meshRenderers[i].material.color.b + 0.01f);
+            if (_meshRenderers[i].materials[_materialIndex].color.r - _deafultColors[i].r < 0.3f) 
+                _meshRenderers[i].materials[_materialIndex].color = 
+                    new Color(_meshRenderers[i].materials[_materialIndex].color.r 
+                    + 0.01f, _meshRenderers[i].materials[_materialIndex].color.g 
+                    + 0.01f, _meshRenderers[i].materials[_materialIndex].color.b + 0.01f);
         }
     }
 
@@ -41,7 +45,7 @@ public class OutlineObject : MonoBehaviour
     {
         for (int i = 0; i < _meshRenderers.Length; i++)
         {
-            _meshRenderers[i].material.color = _deafultColors[i];
+            _meshRenderers[i].materials[_materialIndex].color = _deafultColors[i];
         }
     }
 }
